0ab4aa0c333ba0124876234a9590c41ef78827d9
El Dorado <tamimrahman7b@gmail.com>
2025-08-25 00:15:33 +0600

    Admin Dashboard Created

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['user']) || $_SESSION['user']['role']!='admin') {
 <h2>Admin Dashboard</h2>
 <p>Welcome, <?php echo $_SESSION['user']['username']; ?>!</p>
 <ul>
-  <li><a href="admin_create_user.php">Create User</a></li>
-  <li><a href="admin_manage_users.php">Manage Users</a></li>
+  <li><a href="admin_create_user.php">Create User Profile</a></li>
+  <li><a href="admin_manage_users.php">Manage Users Profile</a></li>
   <li><a href="logout.php">Logout</a></li>
 </ul>
